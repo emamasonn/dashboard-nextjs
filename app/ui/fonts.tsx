b7922688a1a0d1744fe765a5972f29b0e029d3a8
David Mason <emamasonn@gmail.com>
2024-01-11 11:06:52 -0300

    initial project

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import { Lusitana, Inter } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export const inter = Inter({
+  weight: '400',
+  subsets: ['latin'],
+});
